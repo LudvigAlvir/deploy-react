cc30a028eba0f5073b60922c50f54d5e93cf6d55
Ludvig Alvir <ludvi@OSL_Ludvig_FED1.res.bitpro.as>
2025-02-27 09:55:13 +0100

    add links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <h1>I have sucsesfully deployed</h1>
+      <h1>I have succesfully deployed</h1>
+      <Link to={"/test"}>To test page</Link>
+      <br />
+      <a href="/test">Anchor to test page</a>
     </>
   );
 }
